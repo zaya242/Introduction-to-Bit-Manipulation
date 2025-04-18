9c2b946693625809b8064b817bd721b993e2863f
zaya242 <isiomajadesimi@gmail.com>
2025-04-18 18:03:47 +0100

    activity1

---
+++ activity1.py
@@ -0,0 +1,28 @@
+#program to showcase working of different types of bitwise operators
+
+num1 = 10
+num2 = 4
+
+#using AND operator
+print("num1 & num2 =", num1 & num2)
+
+#using OR operator
+print("\nnum1 | num2 =", num1 | num2)
+
+#using NOT operator
+print("\n~num1 =", ~num1)
+
+#using XOR operator
+print("\nnum1 ^ num2 =", num1 ^ num2)
+
+#using right shift operator on num1
+print("\nnum1 >> 1 =", num1 >> 1)
+
+#using right shift operator on num2
+print("\nnum2 >> 1 =", num2 >> 1)
+
+#using left shift operator on num1
+print("\nnum1 << 1 =", num1 << 1)
+
+#using left shift operator on num2
+print("\nnum2 << 1 =", num2 << 1)
